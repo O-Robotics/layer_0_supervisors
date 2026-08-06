68b8a8b9370efcba9f885c004d73f90244d01716
Oleksiy Dorokhov <223628935+OleksiyDorokhov@users.noreply.github.com>
2026-08-06 10:38:51 +0200

    Updated application version in cloud-listener.hpp.

--- amr_sweeper_cloud_listener/cloud-listener.hpp
+++ amr_sweeper_cloud_listener/cloud-listener.hpp
@@ -23,7 +23,7 @@
 #error "Azure IoT C SDK headers not found. Install azure-iot-sdk-c or set CPPFLAGS."
 #endif
 
-#define VERSION "0.1 pre-alfa"
+#define VERSION "0.7 alfa"
 
 #define IDLING 101
 #define WORKING 201
